f08d66e55745e4d553ab55cf3d3bfc4c05eda972
Igor Zolotarev <yngvar.antonsson@gmail.com>
2018-12-27 03:37:28 +0300

    File name fixed

--- music.py
+++ music.py
@@ -1,15 +1,15 @@
-from os import getcwd
-import glob
+import os
+from glob import glob
 from mutagen.easyid3 import EasyID3
 
-fpath = f'{getcwd()}/*.mp3'
-files = glob.glob(fpath)
+fpath = f'{os.getcwd()}/*.mp3'
+files = glob(fpath)
 
 for fname in files:
     if fname.find('_') != -1:
         print(f'Error in file {fname}')
         continue
-    name_list = fname.split('-')
+    name_list = os.path.basename(fname)[:-4].split('-')
     if len(name_list) > 2:
         print(f'Error in file {fname}')
         continue
