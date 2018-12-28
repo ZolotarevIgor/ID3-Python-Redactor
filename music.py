f411097485948f343975f42392286fb11f4cd3dd
Igor Zolotarev <yngvar.antonsson@gmail.com>
2018-12-28 19:30:41 +0300

    No ID3 header error fixed

--- music.py
+++ music.py
@@ -1,29 +1,41 @@
 import os
 from glob import glob
-from mutagen.easyid3 import EasyID3
+from mutagen.mp3 import MP3
+from mutagen import MutagenError
 
-fpath = f'{os.getcwd()}/*.mp3'
+fpath = f"{os.getcwd()}/*.mp3"
 files = glob(fpath)
 
 for fname in files:
     temp_fname = os.path.basename(fname)[:-4]
-    while temp_fname.find('_') != -1:
-        print(f'Type symbol instead of \'_\' in {fname}')
+    while temp_fname.find("_") != -1:
+        print(f"Type symbol instead of '_' in {fname}")
         symb = None
         type(symb)
         temp_fname.replace('_', symb, 1)
     name_list = temp_fname.split(' - ')
     if len(name_list) > 2:
-        print(f'Error in file {fname}')
+        print(f"Error parsing file name: {fname}")
         continue
     artist, song_name = name_list
-    tag = EasyID3(fname)
     try:
-        tag['artist']
+        mp3 = MP3(fname)
+    except MutagenError:
+        print(f"File loading error: {fname}")
+        continue
+    if mp3.tags is None:
+        mp3.add_tags()
+    tag = mp3.tags
+    try:
+        tag["artist"]
     except KeyError:
-        tag['artist'] = artist
+        tag["artist"] = artist
     try:
-        tag['title']
+        tag["title"]
     except KeyError: 
-        tag['title'] = song_name
-    tag.save(v2_version=3)
+        tag["title"] = song_name
+    try:
+        tag.save(v2_version=3)
+    except MutagenError:
+        print(f"File saving error: {fname}")
+        continue
