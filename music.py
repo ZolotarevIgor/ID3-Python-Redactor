b4b701169676ddae2699315e0b51341c9e260a3d
Igor Zolotarev <yngvar.antonsson@gmail.com>
2019-01-13 04:13:40 +0300

    Tag addition fixed

--- music.py
+++ music.py
@@ -1,18 +1,18 @@
-import os
+from os import getcwd
+from os.path import basename
 from glob import glob
 from mutagen.mp3 import MP3
-from mutagen import MutagenError
+from mutagen.id3 import TPE1, TIT2
 
-fpath = f"{os.getcwd()}/*.mp3"
+fpath = f"{getcwd()}/*.mp3"
 files = glob(fpath)
 
 for fname in files:
-    temp_fname = os.path.basename(fname)[:-4]
+    temp_fname = basename(fname)[:-4]
     while temp_fname.find("_") != -1:
-        print(f"Type symbol instead of '_' in {fname}")
-        symb = None
-        type(symb)
-        temp_fname.replace('_', symb, 1)
+        print(f"Type symbol instead of '_' in {temp_fname}")
+        symb = input()
+        temp_fname = temp_fname.replace('_', symb, 1)
     name_list = temp_fname.split(' - ')
     if len(name_list) > 2:
         print(f"Error parsing file name: {fname}")
@@ -20,22 +20,18 @@ for fname in files:
     artist, song_name = name_list
     try:
         mp3 = MP3(fname)
-    except MutagenError:
-        print(f"File loading error: {fname}")
+    except BaseException as e:
+        print(f"{fname} loading error: {e}")
         continue
     if mp3.tags is None:
         mp3.add_tags()
     tag = mp3.tags
+
+    if not "artist" in tag:
+        tag.add(TPE1(encoding=3, text=artist))
+    if not "title" in tag:
+        tag.add(TIT2(encoding=3, text=song_name))
     try:
-        tag["artist"]
-    except KeyError:
-        tag["artist"] = artist
-    try:
-        tag["title"]
-    except KeyError: 
-        tag["title"] = song_name
-    try:
-        tag.save(v2_version=3)
-    except MutagenError:
-        print(f"File saving error: {fname}")
-        continue
+        mp3.save(v2_version=3)
+    except BaseException as e:
+        print(f"{fname} saving error: {e}")
